63aa25ce34dbff469299757ccc207846b61cbf84
Surya Kanta Pal <skpeng2005@gmail.com>
2025-06-16 13:59:21 +0530

    Update App.jsx
    for making animation in the homepage

--- App.jsx
+++ App.jsx
@@ -23,27 +23,35 @@
 
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+
 import Home from './pages/Home';
 import CreatorLogin from './pages/CreatorLogin';
 import CreatorDashboard from './pages/CreatorDashboard';
 import Investor from './pages/Investor';
 import Sponsor from './pages/Sponsor';
 
+const AnimatedRoutes = () => {
+  const location = useLocation();
 
-// Add Sponsor if needed
-
-const App = () => {
   return (
-    <Router>
-      <Routes>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Home />} />
         <Route path="/creator" element={<CreatorLogin />} />
         <Route path="/creator/dashboard" element={<CreatorDashboard />} />
         <Route path="/investor" element={<Investor />} />
         <Route path="/sponsor" element={<Sponsor />} />
-        {/* Add sponsor route later */}
       </Routes>
+    </AnimatePresence>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <AnimatedRoutes />
     </Router>
   );
 };
